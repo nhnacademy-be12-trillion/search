6d9470e32f5f6bea340b9d17b47044ecdf00b734
serrot <parksh568923@gmail.com>
2025-12-12 03:29:28 +0900

    인프라 초기 설정 완료!

--- src/main/java/com/nhnacademy/search/SearchApplication.java
+++ src/main/java/com/nhnacademy/search/SearchApplication.java
@@ -1,3 +1,15 @@
+/*
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * + Copyright 2025. NHN Academy Corp. All rights reserved.
+ * + * While every precaution has been taken in the preparation of this resource,  assumes no
+ * + responsibility for errors or omissions, or for damages resulting from the use of the information
+ * + contained herein
+ * + No part of this resource may be reproduced, stored in a retrieval system, or transmitted, in any
+ * + form or by any means, electronic, mechanical, photocopying, recording, or otherwise, without the
+ * + prior written permission.
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ */
+
 package com.nhnacademy.search;
 
 import com.nhnacademy.search.config.BookStatsSyncProperties;
@@ -7,10 +19,12 @@ import com.nhnacademy.search.config.ReviewSummaryPipelineProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 @EnableConfigurationProperties({ElasticsearchProperties.class, GeminiProperties.class, ReviewSummaryPipelineProperties.class, BookStatsSyncProperties.class})
 public class SearchApplication {
 
