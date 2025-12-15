7271ab5b566503053ffc92b5c51395b788e2d0bd
chosun-nhn40 <roben0510@naver.com>
2025-12-15 11:24:20 +0900

    API 컨벤션 적용

--- src/main/java/com/nhnacademy/search/controller/ReviewSummaryController.java
+++ src/main/java/com/nhnacademy/search/controller/ReviewSummaryController.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/books")
 @RequiredArgsConstructor
 @Tag(name = "Reviews", description = "리뷰 요약 API")
 public class ReviewSummaryController {
