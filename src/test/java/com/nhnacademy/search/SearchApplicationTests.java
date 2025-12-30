27a5a83dbf5de7508c5ec902b6308e34e14b8b4b
chosun-nhn40 <roben0510@naver.com>
2025-12-30 23:12:24 +0900

    sonarqube 설정

--- src/test/java/com/nhnacademy/search/SearchApplicationTests.java
+++ src/test/java/com/nhnacademy/search/SearchApplicationTests.java
@@ -1,12 +1,19 @@
 package com.nhnacademy.search;
 
+import com.nhnacademy.search.schedule.EmbeddingBackfillScheduler;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootTest
 class SearchApplicationTests {
-
+    @MockitoBean
+    private WebClient elasticsearchWebClient;
+    @MockitoBean
+    private EmbeddingBackfillScheduler scheduler;
     @Test
     void contextLoads() {
+
     }
 }
