3797a5fd7a7ad30c6d41d0d3e960f078bd62fa48
chosun-nhn40 <roben0510@naver.com>
2026-01-05 15:39:16 +0900

    pom.xml 의존성 추가

--- src/main/java/com/nhnacademy/search/service/BookSearchService.java
+++ src/main/java/com/nhnacademy/search/service/BookSearchService.java
@@ -70,8 +70,7 @@ public class BookSearchService {
         Map<String, Object> multiMatch = new HashMap<>();
         multiMatch.put("query", req.query());
         multiMatch.put("type", "best_fields");
-        multiMatch.put("operator", "or");
-        multiMatch.put("minimum_should_match", "1");
+        multiMatch.put("operator", "and");
         multiMatch.put("fields", List.of(
                 "metadata.title^100",
                 "title^100",
